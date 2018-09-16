6af857f8218933b5bdfe41d6031c12bfa05c4c5d
carlos arellano <carlos@Carloss-MacBook-Pro-3.local>
2018-09-15 23:21:01 -0400

    addded php contact form

---
+++ mail.php
@@ -0,0 +1,33 @@
+<?php
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $message = $_POST['subject'];
+    $email = $_POST['email'];
+    $from = 'From: Demo';
+    $to = 'carellano96@gmail.com';
+    $human = $_POST['human'];
+    $subject = 'Hello';
+    $body= "From: $name\n Email: $email\n Message:$message";
+
+    if ($_POST['submit']){
+        if ($name !=''&&$email !=''){
+            if ($human=='5'){
+                if (mail($to,$subject,$body,$from)){
+                    echo '<p> Your message has been sent!</p>';
+                }
+                else{
+                    echo '<p>Something went wrong. Please go back and try again!</p>';
+
+                }
+            }
+            else if ($_POST['submit']&& $human!='5'){
+                echo '<p>You answered the anti-spam question wrong!</p>';
+
+            }
+        }
+        else{
+            echo '<p> You need to fill in all fields!</p>';
+
+        }
+    }
+?>
